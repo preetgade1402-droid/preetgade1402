7b1dc376b3883c47e4506b34e9facb86f16bf7ff
preetgade1402-droid <preetgade1402@gmail.com>
2026-06-28 00:21:19 +0530

    Change hero text to 'NEER SHAKTI 2.0'
    Updated the hero section text to reflect new branding.

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -13,7 +13,7 @@ export default function HeroHome() {
               className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              AI-driven tools for product teams
+              NEER SHAKTI 2.0
             </h1>
             <div className="mx-auto max-w-3xl">
               <p
@@ -21,8 +21,7 @@ export default function HeroHome() {
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-                Our landing page template works on all devices, so you only have
-                to set it up once, and get beautiful results forever.
+               "Engineered to Innovate. Built to Dominate."
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
